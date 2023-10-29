497bfef2fafa57f46373c8fac8936cc616b755f7
Piotr Grzegorczyk <peter.grzegorczyk@gmail.com>
2023-10-29 13:52:23 +0100

    Revert "added shadcn base"
    This reverts commit b31d04d96e5a4bf732d28cbcb31333d7cbd69302.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { DM_Serif_Display, DM_Sans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const dmSerif = DM_Serif_Display({
-  weight: ["400"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  variable: "--font-dm-serif",
-});
-const dmSans = DM_Sans({
-  weight: ["400", "500", "600", "700", "800", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  variable: "--font-dm-sans",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${dmSerif.variable} ${dmSans.variable} font-sans`}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

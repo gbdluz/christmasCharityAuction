313751ed66fc82fa24320f4c497b754ac299bd0d
gbdluz <gbdluzewski@gmail.com>
2023-11-20 22:17:01 +0100

    Added Discord authentication backend

--- backend/src/apps/authentication/views.py
+++ backend/src/apps/authentication/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.discord.views import DiscordOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
+
+class DiscordLogin(SocialLoginView):
+    adapter_class = DiscordOAuth2Adapter
+    callback_url = "http://127.0.0.1:3000/"
+    client_class = OAuth2Client

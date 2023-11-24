aa8f0f9fc9395c771f1ce00bc7b2c69b22a09ec5
Kamil Witek <kamilprogramuje@gmail.com>
2023-11-24 21:41:12 +0100

    search by input and sort by chosen options works fine

--- frontend/src/app/auction/create/page.tsx
+++ frontend/src/app/auction/create/page.tsx
@@ -60,67 +60,124 @@ const Page = () => {
   };
 
   return (
-    <div className="flex justify-center">
-      <Card className="lg:w-2/3">
-        <CardHeader>
-          <CardTitle>Dodaj ofertę</CardTitle>
-          <CardDescription>
-            Możesz zaoferować usługę lub dodać jakiś produkt
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Tytuł</FormLabel>
-                    <FormControl>
-                      <Input placeholder="wprowadź tytuł" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Opis</FormLabel>
-                    <FormControl>
-                      <Input placeholder="wprowadź opis" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="winnersNumber"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Liczba zwycięzców</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="wprowadź liczbę zwycięzców"
-                        type="number"
-                        min="1"
-                        {...field}
+    <Card className="lg:w-2/3">
+      <CardHeader>
+        <CardTitle>Dodaj ofertę</CardTitle>
+        <CardDescription>
+          Możesz zaoferować usługę lub dodać jakiś produkt
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tytuł</FormLabel>
+                  <FormControl>
+                    <Input placeholder="wprowadź tytuł" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Opis</FormLabel>
+                  <FormControl>
+                    <Input placeholder="wprowadź opis" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="winnersNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Liczba zwycięzców</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="wprowadź liczbę zwycięzców"
+                      type="number"
+                      min="1"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="auctionEnd"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Planowany koniec licytacji</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground",
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Wybierz datę</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => isDatedisabled(date)}
+                        initialFocus
                       />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="bidValue"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Minimalna wartość aukcji</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Wprowadź cyfrę"
+                      type="number"
+                      min="1"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="lg:flex lg:justify-between">
               <FormField
                 control={form.control}
-                name="auctionEnd"
+                name="collectionEnd"
                 render={({ field }) => (
                   <FormItem className="flex flex-col">
-                    <FormLabel>Planowany koniec licytacji</FormLabel>
+                    <FormLabel>Termin zebrania środków</FormLabel>
                     <Popover>
                       <PopoverTrigger asChild>
                         <FormControl>
@@ -154,73 +211,14 @@ const Page = () => {
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name="bidValue"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Minimalna wartość aukcji</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Wprowadź cyfrę"
-                        type="number"
-                        min="1"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className="lg:flex lg:justify-between">
-                <FormField
-                  control={form.control}
-                  name="collectionEnd"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      <FormLabel>Termin zebrania środków</FormLabel>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-[240px] pl-3 text-left font-normal",
-                                !field.value && "text-muted-foreground",
-                              )}
-                            >
-                              {field.value ? (
-                                format(field.value, "PPP")
-                              ) : (
-                                <span>Wybierz datę</span>
-                              )}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="start">
-                          <Calendar
-                            mode="single"
-                            selected={field.value}
-                            onSelect={field.onChange}
-                            disabled={(date) => isDatedisabled(date)}
-                            initialFocus
-                          />
-                        </PopoverContent>
-                      </Popover>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="float-right mt-4">
-                  Dodaj
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
+              <Button type="submit" className="float-right mt-4">
+                Dodaj
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 

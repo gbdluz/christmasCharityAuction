7e26bd23a380d8b86fdcb7ea4442973d8dc47bdf
Piotr Grzegorczyk <peter.grzegorczyk@gmail.com>
2023-12-04 23:12:00 +0100

    Remove commented out code in auction.ts

--- frontend/src/lib/types/auction.ts
+++ frontend/src/lib/types/auction.ts
@@ -1,20 +1,3 @@
-// export type Auction = {
-//   id: number;
-//   title: string;
-//   description: string;
-//   photo: string;
-//   num_of_winners: number;
-//   is_paid: boolean;
-//   is_collected: boolean;
-//   user: number;
-//   user_firstname: string;
-//   user_lastname: string;
-
-//   min_bid_value?: number;
-//   auction_end_data?: Date;
-//   deadline?: Date;
-// };
-
 export type Auction = {
   id: number;
   title: string;
